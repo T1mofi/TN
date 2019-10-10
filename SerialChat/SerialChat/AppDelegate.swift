5a070668588e623576150770ff15c152c17d0b03
T1mofi <sikorskitimofei@gmail.com>
2019-10-10 22:01:12 +0300

    Serial Chat initial commit.

---
+++ SerialChat/SerialChat/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SerialChat
+//
+//  Created by Timofei Sikorski on 10/10/19.
+//  Copyright © 2019 SikorskiIT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
