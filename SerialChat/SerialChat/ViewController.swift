bb8aa0ef893a972d820f005b7221274bfbb59825
T1mofi <sikorskitimofei@gmail.com>
2019-10-11 11:53:46 +0300

    Add function with processing added char, and send to output.

--- SerialChat/SerialChat/ViewController.swift
+++ SerialChat/SerialChat/ViewController.swift
@@ -10,10 +10,52 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var inputTextField: NSTextField!
+    @IBOutlet weak var outputTextFiels: NSTextField!
+    
+    @IBAction func inputTextFieldDidEdited(_ sender: Any) {
+        outputTextFiels.stringValue = inputTextField.stringValue
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.waitForInput()
+        }
+        
+    }
+    
+    func waitForInput() {
+        
+        var tempInputText = self.inputTextField.stringValue
+        
+        // Check for input in infinite loop
+        while true {
+            
+            DispatchQueue.main.sync {
+                let currentInputText = self.inputTextField.stringValue
+                
+                // If user input char in middle of string
+                let difference = zip(tempInputText, currentInputText).filter{ $0 != $1 }
+                if !difference.isEmpty {
+                    self.inputTextField.stringValue = tempInputText
+                } else {
+                
+                    if (tempInputText != currentInputText) {
+                        if (currentInputText.count > tempInputText.count) {
+                            let diff = currentInputText[currentInputText.index(before: currentInputText.endIndex)]
+                            
+                            self.outputTextFiels.stringValue += String(diff)
+                        }
+                        tempInputText = currentInputText
+                    }
+                    
+                }
+            }
+            
+        }
+            
     }
 
     override var representedObject: Any? {
